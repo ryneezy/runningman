d975217d723cc6d6899d12d7f2fb01f96425956a
ryneezy <ryneezy@users.noreply.github.com>
2014-05-31 10:56:52 -0700

    Formatting

--- test.sh
+++ test.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+19494194602" -d "Body=s"
+
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900375" -d "Body=ry"
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900376" -d "Body=ab"
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900377" -d "Body=cd"
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900378" -d "Body=ef"
+
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+19494194602" -d "Body=n"
+
+ curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900376" -d "Body=b"
+ curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900377" -d "Body=a"
+ curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900378" -d "Body=a"
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900375" -d "Body=a"
- curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900376" -d "Body=b"
- curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900377" -d "Body=a"
- curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900378" -d "Body=a"
+
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+19494194602" -d "Body=n"
+
+ curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900378" -d "Body=b"
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900375" -d "Body=b"
- curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900376" -d "Body=b"
+ curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900376" -d "Body=a"
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900377" -d "Body=a"
- curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+12132900378" -d "Body=a"
+
  curl -XPOST "http://localhost:8080/runningman" --data-urlencode "From=+19494194602" -d "Body=e"
